9ab799d7223e664e8a5094ec54319b6d696d6d57
Shahul <shahulhameedk479@gmail.com>
2022-06-30 22:45:54 +0530

    08-Sea-Travel-Website-Upto-Scroll-Reveal

--- js/main.js
+++ js/main.js
@@ -33,3 +33,33 @@ navLinks.forEach((link) =>
 	})
 );
 // Scroll Reveal
+const sr = ScrollReveal({
+	distance: "25px",
+	duration: 2500,
+});
+
+sr.reveal(`.image-left, .popular-destinations-data, .plan-trip-data`, {
+	origin: "left",
+});
+
+sr.reveal(".image-center", {
+	origin: "bottom",
+});
+
+sr.reveal(`.image-right, .showcase-data`, {
+	origin: "right",
+});
+
+sr.reveal(".beach-data-wrapper", {
+	origin: "bottom",
+	interval: 250,
+});
+
+sr.reveal(".newsletter-container", {
+	origin: "top",
+});
+
+sr.reveal(`.footer-container-inner > div, .footer-seperator, .copyright`, {
+	origin: "bottom",
+	interval: "250",
+});
